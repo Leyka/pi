dc6a771f9f648e353470ce10a410a8a68872d013
Skander <skander.kc@gmail.com>
2022-06-05 23:11:54 -0400

    Setup basic zsh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ sudo timedatectl set-timezone America/Toronto
 sudo apt update -qq && sudo apt upgrade -yqq
 packages=(
   bat
-  build-essential
   curl
   fail2ban
   htop
@@ -32,8 +31,17 @@ sudo ufw allow from 192.168.1.0/24
 sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 sudo service fail2ban restart
 
+# Oh-my-zsh & plugins
+[ ! -d ~/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ] && git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+[ ! -f ~/.hushlogin ] && touch ~/.hushlogin
+
+# Z
+[ ! -f ~/.z ] && wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ~/.z
+
 # Zsh
 sudo chsh -s "$(which zsh)"
-
-# Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm -f ~/.zshrc
+stow zsh
+source ~/.zshrc
---
+++ zsh/.zshrc
@@ -0,0 +1,10 @@
+plugins=(
+  common-aliases
+  colorize
+  git
+  z
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+)
+
+source ~/.oh-my-zsh/oh-my-zsh.sh
